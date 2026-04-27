4009dbdfbf8d607df2bddebb409980e40c6466d2
zhaoyafei11 <zhaoyafei11@jd.com>
2026-04-27 18:02:17 +0800

    GLM5.1 version

--- backend/codePilot-api/build.gradle.kts
+++ backend/codePilot-api/build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
 
 dependencies {
     api(project(":codePilot-core"))
+    api(project(":codePilot-gateway"))
 
     implementation("org.springframework.boot:spring-boot-starter-webflux")
     implementation("org.springframework.boot:spring-boot-starter-validation")
